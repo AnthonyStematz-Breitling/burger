fe2498f17d3d6a468e836e666f8498ee8a6cdbc5
AnthonyStematz-Breitling <stembreit@gmail.com>
2020-09-08 16:11:17 -0500

    added values to db using seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("butter burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon-deluxe burger", false); 
+INSERT INTO burgers (burger_name, devoured) VALUES  ("cheese burger", true);
