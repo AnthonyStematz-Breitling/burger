78109dc59fd72d77098c5f178c7fd4b61725d1a9
AnthonyStematz-Breitling <stembreit@gmail.com>
2020-09-10 10:17:16 -0500

    attempted to write models

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,21 @@
 var orm = require("../config/orm.js")
 
-//module.exports = burgers
+var burgers = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    },
+    insertOne: function(name, cb){
+        orm.insertOne("burgers", "burger_name", "devoured", name, "false", function(res){
+            cb(res);
+        });
+    },
+    updateOne: function(name, cb){
+        orm.updateOne("burgers", "devour", "burger_name", name, function(res){
+            cb(res);
+        });
+    }
+
+};
+module.exports = burgers;
