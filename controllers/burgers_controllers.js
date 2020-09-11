234eca3781e839846f156d98fccce8e04bfc482f
AnthonyStematz-Breitling <stembreit@gmail.com>
2020-09-11 11:44:27 -0500

    wrote routes for insertOne and updateOne inside controllers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,19 +6,23 @@ var router = express.Router();
 // Basic route that sends the user first to the AJAX Page
 router.get("/", function(req, res) {
     burgers.selectAll(function(result){
-        res.render("index", {burgers: result})
+        res.render("index", {burgers: result});
     })
     //starting page
 });
 
 router.get("/api/:new-burger", function(req, res) {
-    burgers.insertOne()
-   //insert button 
+    burgers.insertOne(["burger_name", "devoured"], [req.params.name, false], function(result){
+        res.render("index",{burgers: result});
+    })
+   //create button 
 });
 
 router.post("/api/:eaten-burger", function(req, res) {
-    burgers.updateOne()
+    burgers.updateOne({devoured: true}, "burger_name", req.params.name, function(result){
+        res.render("index", {burgers: result});
+    })
     //update button 
 });
 
-module.exports = router
+module.exports = router;
