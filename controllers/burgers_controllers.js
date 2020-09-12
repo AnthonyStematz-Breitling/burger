05cee369d34333f3357ae51bba040999939ce8ba
AnthonyStematz-Breitling <stembreit@gmail.com>
2020-09-11 19:22:57 -0500

    debugged issue with partials path and partials name.

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -8,21 +8,18 @@ router.get("/", function(req, res) {
     burgers.selectAll(function(result){
         res.render("index", {burgers: result});
     })
-    //starting page
 });
 
 router.get("/api/:new-burger", function(req, res) {
     burgers.insertOne(["burger_name", "devoured"], [req.params.name, false], function(result){
         res.render("index",{burgers: result});
     })
-   //create button 
 });
 
 router.post("/api/:eaten-burger", function(req, res) {
     burgers.updateOne({devoured: true}, "burger_name", req.params.name, function(result){
         res.render("index", {burgers: result});
-    })
-    //update button 
+    }) 
 });
 
 module.exports = router;
--- public/burger_index.js
+++ public/burger_index.js
@@ -1,37 +1,38 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".eaten").on("click", function(event) {
+    $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
-      var newEaten = $(this).data("neweaten");
+      var newDevoured = $(this).data("newdevoured");
   
-      var newEatenState = {
-        sleepy: newEaten
+      var newDevouredState = {
+        devoured: newDevoured
       };
   
       // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newEatenState
+        data: newDevouredState
       }).then(
         function() {
-          console.log("changed eaten to", newSleep);
+          console.log("changed devoured to", newDevoured);
           // Reload the page to get the updated list
           location.reload();
         }
       );
     });
+
   
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      var newBurger = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=eaten]:checked").val().trim()
+      var newburger = {
+        burger_name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
       };
   
       // Send the POST request.
-      $.ajax("/api/burgers", {
+      $.ajax("/api/burgerss", {
         type: "POST",
         data: newBurger
       }).then(
@@ -42,5 +43,4 @@ $(function() {
         }
       );
     });
-   
-  });
+});
